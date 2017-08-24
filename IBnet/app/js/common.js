93a42f5b79a464ac3b4ae35a6f54b095a9c8452f
Oleksandr Matsibura <sashaqwert94@mail.ru>
2017-08-24 20:30:31 +0300

    create project

--- IBnet/app/js/common.js
+++ IBnet/app/js/common.js
@@ -1,30 +1,7 @@
-    document.body.style.background = 'url(img/3.jpg) no-repeat';
-    (function() {
-    var i = 0;
-    var src = [
-        'img/2.jpg',
-        'img/5.jpg'
-    ];
-    var l = src.length;
-    var t;
-    
-    for(i = 0; i < l; i++) {
-        var img = new Image();
-        img.src = src[i];
-        img.onload = function() {
-            $(this).fadeOut();
-        }
-    }
-    
-    i = 0;
-    t = setInterval(function() {
-        if(i === l){
-            i = 0;
-        }
-        document.body.style.background = 'url(' + src[i] + ')no-repeat';
-        document.body.style.backgroundSize = 'cover';
-        document.body.style.overflow = 'hidden';
-        i++;
-    }, 6000);
-    })();    
+$(document).ready(function() {
+  $('.menu').click(function() {
+    $('nav ul').toggle();
+  });//end slide toggle
+  
+});
 
